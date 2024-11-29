b0125a4faab003fd244595e6e40d4639eb9ffbe6
jeflecias <jefleciasg@gmail.com>
2024-11-30 01:10:56 +0800

    error checks 2

--- personal_info_collector.py
+++ personal_info_collector.py
@@ -9,17 +9,21 @@ def check_special_char(given):
         if i in spec_char:
             return False
     return True
-        
+
+available_btypes = ["A+","A-","B+","B-","AB+","AB-","O"]
+yes_no = ["yes","no"]
 people_array = []
 
 #keeps the while loop running
 cont_1 = "yes"
 while cont_1 == "yes":
     # ask the user for their info!!!!
+    # ask name
     name = input("What is your name? ")
     while not check_special_char(name):
         name = input("Please enter a valid name! ")
 
+    # ask the user for age
     while True:
         try:
             age = int(input("What is your age? "))
@@ -28,16 +32,61 @@ while cont_1 == "yes":
             break
         except:
             print("Please enter a valid age!")
+    
+    # ask the user for birthmonth
+    while True:
+        try:
+            birthday_month = int(input("When is your birth month? "))
+            if birthday_month < 1 or birthday_month > 12:
+                raise
+            break
+        except:
+            print("Please enter a valid month!")
+    
+    # ask the user for birth day
+    while True:
+        try:
+            birth_day = int(input("When is your birth day? "))
+            if birth_day < 1 or birth_day > 31:
+                raise
+            break
+        except:
+            print("Please enter a valid day!")
 
-    birthday = input("When is your birthday? ")
+    # ask the user for birth year
+    while True:
+        try:
+            birthday_year = int(input("When is your birth year? "))
+            if birthday_year < 0 or birthday_year > 2024:
+                raise
+            break
+        except:
+            print("Please enter a valid year!")
 
+    # format birthday
+    birthday = str(birthday_month) + "/" + str(birth_day) + "/" + str(birthday_year)
+
+    # ask for blood type
     blood_type = input("What is your blood type? ")
+    while not blood_type.upper().strip() in available_btypes:
+        blood_type = input("Please enter a valid blood type!")
+    
+    # ask if the user has a criminal record
+    criminal_record = input("Do you have a criminal record? yes/no ")
+    while not criminal_record.lower().strip() in yes_no:
+        criminal_record = input("Do you have a criminal record? yes/no ")
 
-    criminal_record = input("Do you have a criminal record? ")
-
-    people_array.append([name,age,birthday,blood_type,criminal_record])
+    people_array.append(
+        ["Name: " + name,
+         "Age: " + str(age),
+         "Birthday: " + birthday,
+         "Blood type: " + blood_type,
+         "Criminal record: " + criminal_record]
+         )
 
     cont_1 = input("Would you like to input another person? yes/no ")
+    while not cont_1.lower().strip() in yes_no:
+        cont_1 = input("Would you like to input another person? yes/no ")
 
 #write file
 with open("list_of_people.txt","w") as file:
