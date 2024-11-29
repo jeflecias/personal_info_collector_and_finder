77f74542a15292fadfccace854e77e3db823445a
jeflecias <jefleciasg@gmail.com>
2024-11-30 00:50:57 +0800

    error checking 1

--- personal_info_collector.py
+++ personal_info_collector.py
@@ -1,7 +1,15 @@
 # program asks the user to input their personal info
 # saves the info into a text file
 
-# this stores the information of multiple people
+# this stores the information of multiple people 
+def check_special_char(given):
+    spec_char = "0123456789!@#$%^&*()[]{}-=_+;\\/:"
+    
+    for i in given:
+        if i in spec_char:
+            return False
+    return True
+        
 people_array = []
 
 #keeps the while loop running
@@ -9,11 +17,26 @@ cont_1 = "yes"
 while cont_1 == "yes":
     # ask the user for their info!!!!
     name = input("What is your name? ")
-    age = input("What is your age? ")
+    while not check_special_char(name):
+        name = input("Please enter a valid name! ")
+
+    while True:
+        try:
+            age = int(input("What is your age? "))
+            if age < 0 or age > 110:
+                raise
+            break
+        except:
+            print("Please enter a valid age!")
+
     birthday = input("When is your birthday? ")
+
     blood_type = input("What is your blood type? ")
+
     criminal_record = input("Do you have a criminal record? ")
+
     people_array.append([name,age,birthday,blood_type,criminal_record])
+
     cont_1 = input("Would you like to input another person? yes/no ")
 
 #write file
