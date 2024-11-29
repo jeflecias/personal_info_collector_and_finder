b68b295e27b4fe292e141defc7d40546d0236bca
jeflecias <jefleciasg@gmail.com>
2024-11-30 00:34:12 +0800

    personal info input

---
+++ personal_info_collector.py
@@ -0,0 +1,17 @@
+# program asks the user to input their personal info
+# saves the info into a text file
+
+# this stores the information of multiple people
+people_array = []
+
+#keeps the while loop running
+cont_1 = "yes"
+while cont_1 == "yes":
+    # ask the user for their info!!!!
+    name = input("What is your name? ")
+    age = input("What is your age? ")
+    birthday = input("When is your birthday? ")
+    blood_type = input("What is your blood type? ")
+    criminal_record = input("Do you have a criminal record? ")
+    people_array.append([name,age,birthday,blood_type,criminal_record])
+    cont_1 = input("Would you like to input another person? yes/no ")
